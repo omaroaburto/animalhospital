60b1a2f89ee8ef11b728efdb048a46d5bd87592b
Omaro Aburto Leiva <oaburto.freelance@gmail.com>
2026-06-23 19:20:05 -0400

    actualización de métodos del modelo user para validación de correo

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,21 +6,25 @@ namespace App\Models;
 
 use App\Enums\Role;
 use Database\Factories\UserFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Support\Str;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable implements JWTSubject, MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, SoftDeletes;
     protected $fillable = [
         'email',
         'password',
-        'role'
+        'role',
+        'email_verified_at',
+        'verification_token',
     ];
     protected $hidden = [
         'password',
@@ -48,5 +52,32 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims():array
     {
         return [];
+    }
+
+    /******************************
+     *   Verificación de correo   *
+     ******************************/
+    public function generateVerificationToken(): string
+    {
+        $token = Str::random(64);
+
+        $this->update([
+            'verification_token' => $token,
+        ]);
+
+        return $token;
+    }
+
+    public function markEmailAsVerified(): void
+    {
+        $this->update([
+            'email_verified_at' => now(),
+            'verification_token' => null,
+        ]);
+    }
+
+    public function hasVerifiedEmailCustom(): bool
+    {
+        return !is_null($this->email_verified_at);
     }
 }
