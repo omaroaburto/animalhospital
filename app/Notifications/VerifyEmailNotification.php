a53d7094abc54dc922882c8d1c063b6b4fca7699
Omaro Aburto Leiva <oaburto.freelance@gmail.com>
2026-06-23 19:28:49 -0400

    creación de notifications para el envió del correo de verificación de email de usuario

---
+++ app/Notifications/VerifyEmailNotification.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class VerifyEmailNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct(public string $token)
+    {
+    }
+
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable): MailMessage
+    {
+        $url = config('app.frontend_url') . '?token=' . $this->token;
+        /*
+        return (new MailMessage)
+            ->subject('Verifica tu cuenta en Animal Hospital')
+            ->greeting('¡Bienvenido!')
+            ->line('Gracias por registrarte. Solo falta un paso.')
+            ->action('Verificar cuenta', $url)
+            ->line('Si no creaste esta cuenta, puedes ignorar este correo.');*/
+         return (new MailMessage)
+            ->subject('Verifica tu cuenta en Animal Hospital')
+            ->view('emails.verify-notification', [
+                'url' => $url,
+                'name' => $notifiable->name // $notifiable es el usuario que recibe el correo
+            ]);
+    }
+}
