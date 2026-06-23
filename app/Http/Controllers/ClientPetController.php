9872226ea83ab95e20ae636771b0a62cabf49781
Omaro Aburto Leiva <oaburto.freelance@gmail.com>
2026-06-23 02:18:32 -0400

    actualización de controlador ClientPetController.php

--- app/Http/Controllers/ClientPetController.php
+++ app/Http/Controllers/ClientPetController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdatePetRequest;
 use App\Http\Resources\PetCollection;
+use App\Http\Resources\PetResource;
 use App\Models\Client;
+use App\Models\Pet;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class ClientPetController extends Controller
 {
@@ -11,5 +16,32 @@ class ClientPetController extends Controller
     {
         $pets =  $client->pets()->with(['breed.species'])->paginate(5);
         return new PetCollection($pets);
+    }
+    public function show(Client $client, Pet $pet): PetResource
+    {
+        $pet->load(['breed.species', 'client.user']);
+        return new PetResource($pet);
+    }
+    public function patch(
+        UpdatePetRequest $request,
+        Client $client,
+        Pet $pet): PetResource
+    {
+        //validamos datos
+        $validatedData =  $request->validated();
+        // Protegemos el controlador eliminando explícitamente el client_id por seguridad
+        unset($validatedData['client_id']);
+        //actualizamos
+        $pet->update($validatedData);
+        $pet->load('breed.species', 'client.user');
+        return new PetResource($pet);
+    }
+    public function destroy(Client $client, Pet $pet): JsonResponse
+    {
+        $pet->delete();
+        return response()->json([
+            'status'  => 'success',
+            'message' => 'La mascota fue eliminada correctamente.'
+        ], Response::HTTP_OK);
     }
 }
